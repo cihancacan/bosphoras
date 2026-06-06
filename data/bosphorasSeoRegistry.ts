107ed2f861b85e4e5fcee93c4839df3eff732b0c
cihancacan <130900341+cihancacan@users.noreply.github.com>
2026-06-06 12:43:15 +0500

    Include visa residence SEO pages in Bosphoras registry

--- data/bosphorasSeoRegistry.ts
+++ data/bosphorasSeoRegistry.ts
@@ -1,7 +1,9 @@
 import { bosphorasProgrammaticSeoPages } from './bosphorasProgrammaticSeoPages';
 import { bosphorasAdditionalSeoPages } from './bosphorasAdditionalSeoPages';
+import { bosphorasVisaResidenceSeoPages } from './bosphorasVisaResidenceSeoPages';
 
 export const allBosphorasSeoPages = [
   ...bosphorasProgrammaticSeoPages,
   ...bosphorasAdditionalSeoPages,
+  ...bosphorasVisaResidenceSeoPages,
 ];
