72cda89db3b6620df2be5e8dbfffb8cd6248101f
cihancacan <130900341+cihancacan@users.noreply.github.com>
2026-06-06 14:09:21 +0500

    Include country-specific visa pages in SEO registry

--- data/bosphorasSeoRegistry.ts
+++ data/bosphorasSeoRegistry.ts
@@ -1,9 +1,11 @@
 import { bosphorasProgrammaticSeoPages } from './bosphorasProgrammaticSeoPages';
 import { bosphorasAdditionalSeoPages } from './bosphorasAdditionalSeoPages';
 import { bosphorasVisaResidenceSeoPages } from './bosphorasVisaResidenceSeoPages';
+import { bosphorasCountryVisaSeoPages } from './bosphorasCountryVisaSeoPages';
 
 export const allBosphorasSeoPages = [
   ...bosphorasProgrammaticSeoPages,
   ...bosphorasAdditionalSeoPages,
   ...bosphorasVisaResidenceSeoPages,
+  ...bosphorasCountryVisaSeoPages,
 ];
