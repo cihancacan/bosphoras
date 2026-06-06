01be4f33982b6f861ffed113326d60580b81cecd
cihancacan <130900341+cihancacan@users.noreply.github.com>
2026-06-06 14:56:32 +0500

    Include visa type pages in SEO registry

--- data/bosphorasSeoRegistry.ts
+++ data/bosphorasSeoRegistry.ts
@@ -2,10 +2,12 @@ import { bosphorasProgrammaticSeoPages } from './bosphorasProgrammaticSeoPages';
 import { bosphorasAdditionalSeoPages } from './bosphorasAdditionalSeoPages';
 import { bosphorasVisaResidenceSeoPages } from './bosphorasVisaResidenceSeoPages';
 import { bosphorasCountryVisaSeoPages } from './bosphorasCountryVisaSeoPages';
+import { bosphorasVisaTypeSeoPages } from './bosphorasVisaTypeSeoPages';
 
 export const allBosphorasSeoPages = [
   ...bosphorasProgrammaticSeoPages,
   ...bosphorasAdditionalSeoPages,
   ...bosphorasVisaResidenceSeoPages,
   ...bosphorasCountryVisaSeoPages,
+  ...bosphorasVisaTypeSeoPages,
 ];
